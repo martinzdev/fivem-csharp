c029f704d1396761675d260f1733f37e71dfebee
Lucas Martins <lucasowza@hotmail.com>
2025-08-08 12:14:49 -0400

    Update fxmanifest.lua: Replace placeholder author and description with actual values for the CitizenFX core

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,12 @@
 fx_version 'bodacious'
 game 'gta5'
 
+author 'martinzdev'
+version '1.0.0'
+description 'Core for CitizenFX'
+
 file 'bin/client/**/*.dll'
 
 client_script 'bin/client/**/*.net.dll'
 server_script 'bin/server/**/*.net.dll'
 
-author 'You'
-version '1.0.0'
-description 'Example Resource from C# Template'
